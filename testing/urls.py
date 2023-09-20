e71280e9c16b3cd68c152d7efde036719b35739f
CHERUKURU INDRASENA REDDY <103891673+Indra211@users.noreply.github.com>
2023-09-20 10:18:21 +0530

    Update urls.py

--- testing/urls.py
+++ testing/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from Demo.views import DetialsList,get_slots
+from Demo.views import DetialsList,SlotsList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('details', DetialsList.as_view()),
-    path('slots',get_slots())
+    path('slots',SlotsList.as_view())
 ]
