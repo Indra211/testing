9750cd24741b0241062db57f2300ca4dea01df29
CHERUKURU INDRASENA REDDY <indrasena0018@gmail.com>
2023-09-20 09:54:10 +0530

    added url

--- testing/urls.py
+++ testing/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from Demo.views import DetialsList
+from Demo.views import DetialsList,get_slots
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('details', DetialsList.as_view())
-
+    path('details', DetialsList.as_view()),
+    path('slots',get_slots())
 ]
